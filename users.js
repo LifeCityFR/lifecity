b2c9db9fc8827470b32a0feff18c9aef3687c0ed
LifeCityFR <concours.agentchaotique@gmail.com>
2024-09-14 20:23:03 +0200

    Add files via upload
    Signed-off-by: LifeCityFR <concours.agentchaotique@gmail.com>

---
+++ users.js
@@ -0,0 +1,200 @@
+[
+    {
+        "username": "Noobdu89",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "N0t00x",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "krazy",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "citrouillea",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "HCB_Eco",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "kaisfotgaga",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "vodor_21",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "vodor_21",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "vodor_21",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "vodor_21",
+        "sanction": "Bannissement",
+        "duree": "Permanent",
+        "raison": "Multiples avertissements"
+    },
+ {
+        "username": "Vince_0758",
+        "sanction": "Bannissement",
+        "duree": "Permanent",
+        "raison": "Freekill répétés et autres raisons"
+    },
+ {
+        "username": "Xenonlebg705",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "Bapt9250",
+        "sanction": "Bannissement",
+        "duree": "1j",
+        "raison": "n/a"
+    },
+ {
+        "username": "erenkutlu27",
+        "sanction": "Bannissement",
+        "duree": "1j",
+        "raison": "n/a"
+    },
+ {
+        "username": "node_desele",
+        "sanction": "Bannissement",
+        "duree": "Permanent",
+        "raison": "Freekill Admin"
+    },
+ {
+        "username": "LeRat_TPL",
+        "sanction": "Bannissement",
+        "duree": "Permanent",
+        "raison": "Freekill Admin"
+    },
+ {
+        "username": "trollyfacecult",
+        "sanction": "Bannissement",
+        "duree": "Permanent",
+        "raison": "Freekill Admin"
+    },
+ {
+        "username": "Fabrice",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "Tw_Zoro",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "angeloleboss21",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "SuperHmarr",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "RxzPRIMEE",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "Le_C",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "Tintin",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "ramini911",
+        "sanction": "Bannissement",
+        "duree": "2j",
+        "raison": "Insulte Admin"
+    },
+ {
+        "username": "Fabrice",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "Tw_zoro",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "Abusing cuff"
+    },
+ {
+        "username": "A618",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "syyriana",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "syyriana",
+        "sanction": "Bannissement",
+        "duree": "1s",
+        "raison": "Insultes répétés"
+    },
+ {
+        "username": "anna_nask02",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    },
+ {
+        "username": "WIBEE",
+        "sanction": "Démission de fonction",
+        "duree": "n/a",
+        "raison": "Abus de pouvoir"
+    },
+{
+        "username": "Tintin",
+        "sanction": "Avertissement",
+        "duree": "n/a",
+        "raison": "n/a"
+    }
+]
